55e79b96f807f6ee21856dcbdb813698995c99fe
Yanako Lajara <yanakolajara@pursuit.org>
2023-08-16 13:30:54 -0400

    Added login and signup routes to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import './App.css';
 import Homepage from './components/Homepage';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
     </div>
   );
