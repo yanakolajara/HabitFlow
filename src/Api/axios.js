d92f9b3c6e4a038817a4ed75dd645b3630b2e3ff
Yanako Lajara <yanakolajara@pursuit.org>
2023-08-21 16:11:28 -0400

    fix

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_KEY,
+    baseURL:
+    process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_KEY
+    : 'DEPLOY URL',
     timeout: 50000,
 });
 
