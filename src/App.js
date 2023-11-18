6cc142eeead4e1e12e516420e2c14d67d9318bfc
Yanako Lajara <yanakolajara@pursuit.org>
2023-11-17 22:54:23 -0500

    Administrators initiative test

--- src/App.js
+++ src/App.js
@@ -1,66 +1,59 @@
-import { Route, Router, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar'
-import './App.css';
-import { UserContext } from './Context/Auth';
-import Homepage from './routes/Homepage';
-import Login from './routes/Login';
-import Signup from './routes/Signup';
-import Dashboard from './routes/Dashboard';
-import { useEffect, useState } from 'react';
-import { verifyToken } from './Api/Api';
-import Calendar from 'react-calendar';
-import Stats from './routes/Stats';
-import NotFound from './routes/NotFound';
-import AddHabit from './routes/AddHabit';
+import { Route, Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { UserContext } from "./Context/Auth";
+import Homepage from "./routes/Homepage";
+import Login from "./routes/Login";
+import Signup from "./routes/Signup";
+import Dashboard from "./routes/Dashboard";
+import { useEffect, useState } from "react";
+import { verifyToken } from "./Api/Api";
+import Calendar from "react-calendar";
+import Stats from "./routes/Stats";
+import NotFound from "./routes/NotFound";
+import AddHabit from "./routes/AddHabit";
+import InternalUser from "./routes/InternalUser";
 
 function App() {
-
-  const [loading, setLoading] = useState(true)
-  const [userAuthInfo, setUserAuthInfo] = useState({})
+  const [loading, setLoading] = useState(true);
+  const [userAuthInfo, setUserAuthInfo] = useState({});
 
   const checkUserAuthStatus = async () => {
     try {
       const userObject = await verifyToken(document.cookie.substring(10));
-      if(userObject.data){
+      if (userObject.data) {
         setUserAuthInfo(userObject.data[0]);
-      }else{
-        setUserAuthInfo({email: null})
+      } else {
+        setUserAuthInfo({ email: null });
       }
-        setLoading(false);
+      setLoading(false);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
+  };
 
   useEffect(() => {
     checkUserAuthStatus();
-  },[])
+  }, []);
 
-
-
-  if(loading){
-    return(
-      <p>Loading...</p>
-    )
+  if (loading) {
+    return <p>Loading...</p>;
   }
 
   return (
-    <div className='app'>
-      <UserContext.Provider
-      value={userAuthInfo}
-      >
-        <Navbar/>
+    <div className="app">
+      <UserContext.Provider value={userAuthInfo}>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Homepage/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/signup' element={<Signup/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/addhabit' element={<AddHabit/>}/>
-          <Route path='/dashboard/:habitId/stats' element={<Stats/>}/>
-          <Route path='*' element={<NotFound/>}/>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/internalUser" element={<InternalUser />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/addhabit" element={<AddHabit />} />
+          <Route path="/dashboard/:habitId/stats" element={<Stats />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-
       </UserContext.Provider>
     </div>
   );
