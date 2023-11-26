2c84fcb65693477fe1e73c9593d924fc8c5abf02
yanakolajara <yanakolajara@pursuit.org>
2023-11-26 00:48:11 -0500

    test

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,122 +1,146 @@
-import Axios from './axios'
+import Axios from "./axios";
 
-async function getAllUsers(){
-    try {
-        const allUsers = await Axios.get('/users')
-        return allUsers;
-    } catch (error) {
-        return error
-    }
+async function getAllUsers() {
+  try {
+    const allUsers = await Axios.get("/users");
+    return allUsers;
+  } catch (error) {
+    return error;
+  }
 }
 
-async function createNewUser(first_name, last_name, dob_day, dob_month, dob_year, gender, email, password){
-    try {
-        const newUser = await Axios.post('/users',{
-            first_name: first_name,
-            last_name: last_name,
-            dob_day: dob_day,
-            dob_month: dob_month,
-            dob_year: dob_year,
-            gender: gender,
-            email: email,
-            password: password
-        })
-        return newUser
-    } catch (error) {
-        return error
-    }
+async function createNewUser(
+  first_name,
+  last_name,
+  dob_day,
+  dob_month,
+  dob_year,
+  gender,
+  email,
+  password
+) {
+  try {
+    const newUser = await Axios.post("/users", {
+      first_name: first_name,
+      last_name: last_name,
+      dob_day: dob_day,
+      dob_month: dob_month,
+      dob_year: dob_year,
+      gender: gender,
+      email: email,
+      password: password,
+    });
+    return newUser;
+  } catch (error) {
+    return error;
+  }
 }
 
-async function login(email, password){
-    try {
-        const user = await Axios.post('/users/login', {
-            email: email,
-            password: password
-        })
-        return user
-    } catch (error) {
-        return error
-    }
+async function login(email, password) {
+  try {
+    const user = await Axios.post("/users/login", {
+      email: email,
+      password: password,
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
 }
 
-async function verifyToken(token){
-    try {
-        const user = await Axios.get(`/users/verify-token/${token}`)
-        return user
-    } catch (error) {
-        return error
-    }
+async function verifyToken(token) {
+  try {
+    const user = await Axios.get(`/users/verify-token/${token}`);
+    return user;
+  } catch (error) {
+    return error;
+  }
 }
 
-async function checkUserHabits(id){
-    try {
-        const userHabits = await Axios.get(`/users/${id}/habits/user-habits`);
-        return userHabits;
-    } catch (error) {
-        return error
-    }
+async function checkUserHabits(id) {
+  try {
+    const userHabits = await Axios.get(`/users/${id}/habits/user-habits`);
+    return userHabits;
+  } catch (error) {
+    return error;
+  }
 }
 
-async function getAllHabits(){
-    try {
-        const allHabits = await Axios.get('/habits')
-        return allHabits;
-    } catch (error) {
-        return error
-    }
+async function getAllHabits() {
+  try {
+    const allHabits = await Axios.get("/habits");
+    return allHabits;
+  } catch (error) {
+    return error;
+  }
 }
 
-async function getHabitById(habitId){
-    try {
-        const habitById = await Axios.get(`/habits/${habitId}`);
-        return habitById;
-    } catch (error) {
-        return error;
-    }
+async function getHabitById(habitId) {
+  try {
+    const habitById = await Axios.get(`/habits/${habitId}`);
+    return habitById;
+  } catch (error) {
+    return error;
+  }
 }
 
-async function addHabitToUser(userId, habitId){
-    try {
-        const habitToUser = await Axios.post(`/users/${userId}/habits/user-habits/${habitId}`)
-        return habitToUser;
-    } catch (error) {
-        return error
-    }
+async function addHabitToUser(userId, habitId) {
+  try {
+    const habitToUser = await Axios.post(
+      `/users/${userId}/habits/user-habits/${habitId}`
+    );
+    return habitToUser;
+  } catch (error) {
+    return error;
+  }
 }
 
-async function getHabitStats(userId, habitId){
-    try {
-        const habitStats = await Axios.get(`/users/${userId}/habits/stats/${habitId}`)
-        return habitStats
-    } catch (error) {
-        return error;
-    }
+async function getHabitStats(userId, habitId) {
+  try {
+    const habitStats = await Axios.get(
+      `/users/${userId}/habits/stats/${habitId}`
+    );
+    return habitStats;
+  } catch (error) {
+    return error;
+  }
 }
 
-async function createHabitStats(user_id, habit_id, day, month, year, completion, progress){
-    try {
-        const newStat = await Axios.post(`/users/${user_id}/habits/stats/${habit_id}`, {
-            "day": day,
-            "month": month,
-            "year": year,
-            "completion": completion,
-            "progress": progress
-        })
-        return newStat
-    } catch (error) {
-        return error;
-    }
+async function createHabitStats(
+  user_id,
+  habit_id,
+  day,
+  month,
+  year,
+  completion,
+  progress
+) {
+  try {
+    const newStat = await Axios.post(
+      `/users/${user_id}/habits/stats/${habit_id}`,
+      {
+        day: day,
+        month: month,
+        year: year,
+        completion: completion,
+        progress: progress,
+      }
+    );
+    return newStat;
+  } catch (error) {
+    return error;
+  }
 }
 
 export {
-    getAllUsers,
-    createNewUser,
-    login,
-    verifyToken,
-    checkUserHabits,
-    getAllHabits,
-    getHabitById,
-    addHabitToUser,
-    getHabitStats,
-    createHabitStats
-}
+  getAllUsers,
+  createNewUser,
+  login,
+  verifyToken,
+  checkUserHabits,
+  getAllHabits,
+  getHabitById,
+  addHabitToUser,
+  getHabitStats,
+  createHabitStats,
+};
