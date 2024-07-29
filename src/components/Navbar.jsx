c8fa1b486a05be8bb095ca9c276164d9a8019c96
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-07-29 14:26:36 -0400

    refactor: navbar rendering structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,35 +10,6 @@ function Navbar() {
   const { logout } = useAuth();
   const navigate = useNavigate();
 
-  function checkUserLogInStatus() {
-    if (email) {
-      return (
-        <div id='navbar-buttons'>
-          <button
-            id='navbar-dashboard-button'
-            onClick={() => navigate('/addhabit')}
-          >
-            Add habit
-          </button>
-          <button id='navbar-account-button' onClick={() => logout()}>
-            Log out
-          </button>
-        </div>
-      );
-    } else {
-      return (
-        <div id='navbar-buttons'>
-          <button id='navbar-signup-button' onClick={() => navigate('/signup')}>
-            Sign up
-          </button>
-          <button id='navbar-login-button' onClick={() => navigate('/login')}>
-            Log in
-          </button>
-        </div>
-      );
-    }
-  }
-
   return (
     <div id='navbar-box'>
       <div className='icon-div'>
@@ -49,7 +20,33 @@ function Navbar() {
           onClick={() => navigate('/')}
         />
       </div>
-      {checkUserLogInStatus()}
+      <div id='navbar-buttons'>
+        {(email && (
+          <>
+            <button
+              id='navbar-dashboard-button'
+              onClick={() => navigate('/addhabit')}
+            >
+              Add habit
+            </button>
+            <button id='navbar-account-button' onClick={() => logout()}>
+              Log out
+            </button>
+          </>
+        )) || (
+          <>
+            <button
+              id='navbar-signup-button'
+              onClick={() => navigate('/signup')}
+            >
+              Sign up
+            </button>
+            <button id='navbar-login-button' onClick={() => navigate('/login')}>
+              Log in
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
